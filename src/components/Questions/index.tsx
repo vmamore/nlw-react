4fd5c3c57434ba0d8179ff1367d423b09cf436e9
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-07-03 19:42:02 -0400

    Finish NLW React project

---
+++ src/components/Questions/index.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react';
+import './styles.scss';
+
+type QuestionProps = {
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+    author: {
+        name: string;
+        avatar: string;
+    };
+    children?: ReactNode;
+}
+
+export function Question({
+    content,
+    author,
+    isAnswered = false,
+    isHighlighted = false,
+    children
+}: QuestionProps) {
+    return (
+        <div className={`question  ${isAnswered ? 'answered': ''} ${isHighlighted ? 'highlighted' : ''}`}>
+            <p>{content}</p>
+            <footer>
+                <div className="user-info">
+                    <img src={author.avatar} alt={author.name} />
+                    <span>{author.name}</span>
+                </div>
+                <div>
+                    {children}
+                </div>
+            </footer>
+        </div>
+    );
+}
